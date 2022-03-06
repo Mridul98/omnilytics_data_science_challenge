3c60eef37c91ed5e965709ad933f758161537ca5
mridul <munmridul@gmail.com>
2022-03-06 13:35:34 +0600

    added solution for part 1 task

--- solutions/results.py
+++ solutions/results.py
@@ -1,8 +1,8 @@
-from time import time
 import pandas as pd
 from kats.consts import TimeSeriesData
 from kats.models.linear_model import LinearModel, LinearModelParams
 from kats.tsfeatures.tsfeatures import TsFeatures
+
 
 TREND_DATASET_PATH = 'datasets/trends1.csv'
 
@@ -10,10 +10,19 @@ TREND_DATASET_PATH = 'datasets/trends1.csv'
 class Result:
 
     def __init__(
-        self,df_path,
+        self,df_path:str,
         date_column_name='date',
         top_column_names=['top 1','top 2','top 3']
     ) -> None:
+        """
+
+        Args:
+            df_path (str): path to csv trend csv files
+            date_column_name (str, optional): name of the date column of the corresponding 
+            csv files containing time series data. Defaults to 'date'.
+            top_column_names (list, optional): list of column name 
+            that represents name of different tops . Defaults to ['top 1','top 2','top 3'].
+        """
 
         self.time_series_df = pd.read_csv(df_path,
                                           parse_dates=[date_column_name],
@@ -21,6 +30,8 @@ class Result:
 
         self.top_column_name_list = top_column_names
         self.date_column_name = date_column_name
+
+        ## stl features
         self.feature_model = TsFeatures(
             selected_features=[
                 'linearity',
@@ -29,8 +40,13 @@ class Result:
         )
 
     @property
-    def preprocessed_dataset(self):
-    
+    def preprocessed_dataset(self) -> pd.DataFrame:
+        """preprocessed time series dataset with resampled values
+        from weekly to monthly total
+
+        Returns:
+            pd.DataFrame: time series data with resampled monthly values
+        """
 
         convert_dtype_dict = {i: float for i in self.top_column_name_list}
         
@@ -39,21 +55,47 @@ class Result:
                                         ascending=True,inplace=True)
         
         time_series_df_resampled = self.time_series_df.set_index('date')\
-                                            .resample('M').mean().reset_index()
+                                            .resample('M').sum().reset_index()
         
         return time_series_df_resampled
 
-        
-    
-    def last_12m(self,top_name:str) -> None:
-        
+    @property
+    def preprocessed_dataset_cumsum(self) -> pd.DataFrame:
+        """preprocessed cumulative sum time series of 
+        tops trends dataset
+
+        Returns:
+            pd.DataFrame: dataframe containing monthly cumulative sum of tops values
+        """
+
+        trend_cumsum_df = self.preprocessed_dataset.set_index(self.date_column_name)\
+                              .cumsum().reset_index()
+
+        return trend_cumsum_df
+
+
+    def __get_time_series_stats(self,top_name:str,month_lag:int) -> dict:
+        """get stl features for the time series of the given top name
+        For now it returns only time series regresssiong coefficients, linearity and trend strength
+
+        Args:
+            top_name (str): _description_
+            month_lag (int): _description_
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            dict: _description_
+        """
         if top_name not in self.top_column_name_list:
             raise ValueError('top name not found in the dataframe')
         else:
-            last_year_df = self.preprocessed_dataset[[self.date_column_name,top_name]].iloc[-12:]
 
+            month_lag_df = self.preprocessed_dataset[[self.date_column_name,top_name]].iloc[-month_lag:]
+            
             ts_last_year_data = TimeSeriesData(
-                df=last_year_df,
+                df=month_lag_df,
                 time_col_name=self.date_column_name
             )
             linear_model = LinearModel(
@@ -68,7 +110,100 @@ class Result:
             
             trend_strength, linearity = ts_features['trend_strength'], ts_features['linearity']
 
-            return f'the slope of the model for {top_name} is {slope} and intercept {intercept} and trend strength {trend_strength} with linearity {linearity}'
+            return {
+
+                'slope' : slope,
+                'intercept' : intercept,
+                'trend_strength' : trend_strength,
+                'linearity' : linearity
+            }
+    
+    def __get_descriptive_stats(self,top_name:str,month_lag:int) -> dict:
+        """get descriptive stats about the tops performance.
+        For now, it is only cumulative growth rate
+
+        Args:
+            top_name (str): _description_
+            month_lag (int): _description_
+
+        Returns:
+            dict: _description_
+        """
+        month_lag_cumsum_df = self.preprocessed_dataset_cumsum.iloc[-month_lag:]
+
+        monthly_cumulative_growth_rate = (
+            (
+                month_lag_cumsum_df[top_name].values[-1]
+                /
+                month_lag_cumsum_df[top_name].values[0]
+            ) 
+            ** (1 / month_lag) - 1
+        ) * 100
+
+        return {
+            "monthly_cumulative_growth_rate" : monthly_cumulative_growth_rate
+        }
+
+
+    def __get_trend_verb(self,slope:float) -> str:
+        """get appropriate string for 
+        representing trend behaviour
+
+        Args:
+            slope (float): slope coefficient of linear regression
+            of univariate time series
+
+        Returns:
+            str: _description_
+        """
+        if slope == 0:
+            return 'no trending'
+
+        if slope < 0:
+            return 'downward trending'
+
+        if slope > 0:
+            return 'upward trending'
+
+
+    def last_12m(self,top_name:str) -> dict:
+        """calculate performance related stats for the given top
+        and return results
+
+        Args:
+            top_name (str): _description_
+
+        Raises:
+            ValueError: raised when the top name is not in the 
+            preprocessed dataframe
+
+        Returns:
+            dict: dictionary containing performance related stats
+        """
+        
+        if top_name not in self.top_column_name_list:
+            raise ValueError('top name not found in the dataframe')
+        else:
+            stl_features = self.__get_time_series_stats(top_name=top_name,month_lag=12)
+            descriptive_stats = self.__get_descriptive_stats(top_name=top_name,month_lag=12)
+
+            final_result = {**stl_features, **descriptive_stats}
+            
+            return final_result
+
+
+    def perf_12m(self,top_name:str):
+        
+        perf_result = self.last_12m(top_name=top_name)
+
+        slope = perf_result['slope']
+        slope_str = "{:.2f}".format(slope)
+        cmgr = "{:.2f}".format(perf_result['monthly_cumulative_growth_rate'])
+
+        result_string = f'''{top_name}'s last year cumulative monthly growth rate is: {cmgr} % with {self.__get_trend_verb(slope)} of co-efficient {slope_str} approx.'''
+        return {
+            top_name : result_string
+        }
 
 
 if __name__ == '__main__':
@@ -76,7 +211,7 @@ if __name__ == '__main__':
     result = Result(df_path=TREND_DATASET_PATH)
     
     for x in ['top 1','top 2','top 3']:
-        print(result.last_12m(top_name=x))
+        print(result.perf_12m(top_name=x))
 
     
 
