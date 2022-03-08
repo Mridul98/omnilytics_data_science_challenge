ccccec16a69f898a4a39dd2a2c77534ca0b064b0
mridul <munmridul@gmail.com>
2022-03-08 20:01:21 +0600

    added rmse and mae calculation

--- solutions/forecast.py
+++ solutions/forecast.py
@@ -1,21 +1,33 @@
 from curses.panel import top_panel
+from statistics import mean
 import pandas as pd
 import argparse
 import plotly.express as px
 from kats.consts import TimeSeriesData
 from kats.models.prophet import ProphetModel, ProphetParams
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 
 DATASET_PATH = './datasets/trends1.csv'
 
 class ProphetForecaster:
+    """Time Series Forecaster with plotting functionalities
+    """
     
     
     def __init__(
         self,df_path:str,
         date_col:str,
         value_col:str,
-        prophet_params: dict
+        prophet_params: ProphetParams
     ):
+        """
+
+        Args:
+            df_path (str): path to csv file
+            date_col (str): name of date column to be used for time series
+            value_col (str): name of value column to be forecasted
+            prophet_params (ProphetParams): parameters that will be passed to prophet model
+        """
         
         self.date_col_name = date_col
         self.value_col_name = value_col
@@ -35,6 +47,13 @@ class ProphetForecaster:
     
     
     def fit(self,pick_last_datapoints_num:int = None):
+        """fit the prophet model on training data
+
+        Args:
+            pick_last_datapoints_num (int, optional): index of the first data points to be chosen for the training.
+            For now, model is fitted 
+            on the entire training set. Defaults to None.
+        """
         
         train_data = None
         self.pick_last_datapoints_num = None
@@ -56,10 +75,17 @@ class ProphetForecaster:
         self.prophet_model.fit()
         
     def predict(self,step=48):
-        
+        """predict using the fitted model
+
+        Args:
+            step (int, optional): number of steps to predict in future. Defaults to 48.
+        """
+        self.prediction_step = step
         self.forecast_df = self.prophet_model.predict(steps=step,include_history=True)
         
     def plot_predictions(self):
+        """plot the prediction along with the historical data 
+        """
         
         identifier_col_name = 'label'
         input_df = self.df
@@ -70,15 +96,6 @@ class ProphetForecaster:
             
             input_df[identifier_col_name].iloc[-self.pick_last_datapoints_num:] = 'train'
         
-        # forecast_renamed = self.forecast_df.rename(
-        #     columns={
-        #         'time': self.date_col_name,
-        #         'fcst' : self.value_col_name
-        #     }
-        # )
-        
-        # forecast_renamed[identifier_col_name] = 'forecasted'
-        
         new_df = pd.concat(
             [
                 input_df,
@@ -86,11 +103,16 @@ class ProphetForecaster:
             ],
             axis=1
         )
-    
+        history_df = new_df.iloc[:-self.prediction_step]
+
+        rmse = mean_squared_error(history_df[self.value_col_name],history_df['fcst'],squared=False)
+        mae = mean_absolute_error(history_df[self.value_col_name],history_df['fcst'])
+        
         fig = px.line(
             new_df,
             x='time',
-            y=[self.value_col_name,'fcst']
+            y=[self.value_col_name,'fcst'],
+            title= f'Using FBProphet RMSE {rmse} and MAE {mae}'
         )
       
         fig.show()
